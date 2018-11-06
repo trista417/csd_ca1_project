3f505d1f6326d0b87f05fe13d74b30965bfc97bc
Trista Monaghan <trista.monaghan@ericsson.com>
2018-11-06 22:44:14 +0000

    Making initial changes to calculate Blood Pressure

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -9,7 +9,8 @@ namespace BPCalculator
         [Display(Name="Low Blood Pressure")] Low,
         [Display(Name="Normal Blood Pressure")]  Normal,
         [Display(Name="Pre-High Blood Pressure")] PreHigh,
-        [Display(Name ="High Blood Pressure")]  High
+        [Display(Name ="High Blood Pressure")]  High,
+        [Display(Name = "Unable to calculate Blood Pressure")] Invalid
     };
 
     public class BloodPressure
@@ -18,6 +19,13 @@ namespace BPCalculator
         public const int SystolicMax = 190;
         public const int DiastolicMin = 40;
         public const int DiastolicMax = 100;
+
+        public const int LowBPDiastolicMax = 60;
+        public const int LowBPSystolicMax = 90;
+        public const int IdealBPDiastolicMax = 80;
+        public const int IdealBPSystolicMax = 120;
+        public const int PreHighBPDiastolicMax = 90;
+        public const int PreHighBPSystolicMax = 140;
 
         [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
         public int Systolic { get; set; }                       // mmHG
@@ -30,8 +38,26 @@ namespace BPCalculator
         {
             get
             {
-                // implement as part of project
-                throw new NotImplementedException("not implemented yet");
+                if (Systolic < LowBPSystolicMax && Diastolic < LowBPDiastolicMax)
+                {
+                    return BPCategory.Low;
+                }
+                else if (Systolic < IdealBPSystolicMax && Diastolic < IdealBPDiastolicMax)
+                {
+                    return BPCategory.Normal;
+                }
+                else if (Systolic < PreHighBPSystolicMax && Diastolic < PreHighBPDiastolicMax)
+                {
+                    return BPCategory.PreHigh;
+                }
+                else if (Systolic >= PreHighBPSystolicMax && Diastolic >= PreHighBPDiastolicMax)
+                {
+                    return BPCategory.High;
+                }
+                else
+                {
+                    return BPCategory.Invalid;
+                }
             }
         }
     }
